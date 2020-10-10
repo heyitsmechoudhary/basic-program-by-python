534fc4a4c316ba0b8391f72647c4f9c9d33bb8e6
Ravi Gupta <61700885+ravi9607@users.noreply.github.com>
2020-10-10 18:37:51 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+# Python Arithmetic Operators( +,-,*,/,%,**,//)
 a=float(input("enter 1st number : "))
 op=input("enter your opration : ")
 b=float(input("enter 2nd number : "))
@@ -9,5 +10,11 @@ elif op=="*":
     print(a*b)
 elif op=="/":
     print(a/b)
+elif op==" % ":
+    print(a%b)  
+elif op==" ** ":
+    print(a**b)  
+elif op==" // ":
+    print(a//b)  
 else:
     print("please enter a valid opration")
